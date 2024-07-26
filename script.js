b4256a240d20b679194efa7bf973b999612823f1
oghovese imetowa <itzoghuski@gmail.com>
2024-07-27 00:25:30 +0100

    added a feature to generate new grid sizes

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ const btn = document.querySelector("#btn")
 btn.addEventListener('click' , () => {
     let grid;
 
+    do {
+        grid = prompt("Enter the number of squares per side for the new grid (maximum 100):");
+
+        if (grid === null) {
+            console.log("User canceled the prompt.");
+            return; // Exit the function if the user cancels the prompt
+        }
+
+        grid = Number(grid);
+
+        if (grid > 0 && grid <= 100) {
+            createGrid(grid);
+        } else {
+            alert("Please enter a number between 1 and 100.");
+        }
+    } while (grid <= 0 || grid > 100);
 })
 
 function createGrid(ratio){
     
+    container.innerHTML = "";
+
     for (let i = 0; i < ratio * ratio ; i++) {
         let box = document.createElement('div')
         box.classList.toggle('box')
@@ -23,4 +41,4 @@ function createGrid(ratio){
     }
 }
 
-createGrid(100)
+createGrid(16)
