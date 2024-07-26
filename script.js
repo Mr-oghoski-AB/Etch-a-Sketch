d1b677bef0eb16a76337776e0c1fb382541c2545
oghovese imetowa <itzoghuski@gmail.com>
2024-07-27 00:09:58 +0100

    created a function that creates the grid boxes

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const  container = document.querySelector('#container')
+const btn = document.querySelector("#btn")
+// let input = document.querySelector("#inp")
 
-let grid = 16 ;
 
-for (let i = 0; i < grid * grid ; i++) {
-    let box = document.createElement('div')
-    box.classList.toggle('box')
-    box.addEventListener('mouseover' , () => {
-        box.style.backgroundColor = 'green';
-    })
-    container.appendChild(box);
+btn.addEventListener('click' , () => {
+    let grid;
+
+})
+
+function createGrid(ratio){
     
+    for (let i = 0; i < ratio * ratio ; i++) {
+        let box = document.createElement('div')
+        box.classList.toggle('box')
+        box.style.width = `calc(100%/${ratio})`
+        box.style.height = `calc(100%/${ratio})`
+        box.addEventListener('mouseover' , () => {
+            box.style.backgroundColor = 'green';
+        })
+        container.appendChild(box);
+        
+    }
 }
+
+createGrid(100)
